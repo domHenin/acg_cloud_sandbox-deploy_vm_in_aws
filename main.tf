bff34a3ff8e598dab373f0b86e177138cb72241c
clxdev <dominic.heningburg@cloudlogix.io>
2022-09-19 16:34:28 -0400

    still wip: check the provisioner block and verify the proper user for instance

--- main.tf
+++ main.tf
@@ -1,4 +1,148 @@
-provider "aws" {}
+provider "aws" {
+  region = var.aws_region
+}
+
+# Create key-pair for logging into EC2
+resource "aws_key_pair" "key_webserver" {
+  key_name   = var.key_name
+  public_key = file("~/.ssh/id_rsa.pub")
+}
+
+#Get Linux AMI ID using SSM Parameter endpoint in us-east-1
+data "aws_ssm_parameter" "webserver_ami" {
+  # name = "ubuntu/images/hvm-ssd/*"
+  name = "/aws/service/canonical/ubuntu/server/20.04/stable/current/amd64/hvm/ebs-gp2/ami-id"
+}
+
+#Create VPC in us-east-1
+resource "aws_vpc" "vpc_webserver" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = var.vpc_tag
+  }
+}
+
+#Create IGW in us-east-1
+resource "aws_internet_gateway" "igw_webserver" {
+  vpc_id = aws_vpc.vpc_webserver.id
+}
+
+
+#Get main route table to modify
+data "aws_route_table" "route_table_main" {
+  filter {
+    name   = "association.main"
+    values = ["true"]
+  }
+  filter {
+    name   = "vpc-id"
+    values = [aws_vpc.vpc_webserver.id]
+  }
+}
+
+#Create route table in us-east-1
+resource "aws_default_route_table" "internet_route" {
+  default_route_table_id = data.aws_route_table.route_table_main.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw_webserver.id
+  }
+
+  tags = {
+    "Name" = "Webserver-RouteTable"
+  }
+}
+
+
+#Get all available AZ's in VPC for master region
+data "aws_availability_zones" "azs" {
+  state = "available"
+}
+
+#Create subnet # 1 in us-east-1
+resource "aws_subnet" "subnet_webserver" {
+  availability_zone = element(data.aws_availability_zones.azs.names, 0)
+  vpc_id            = aws_vpc.vpc_webserver.id
+  cidr_block        = var.subnet_cidr
+}
+
+#Create SG for allowing TCP/80 & TCP/22
+resource "aws_security_group" "sg_webserver" {
+  name        = "webserver-sg"
+  description = "Allow TCP/80 && TCP/22"
+  vpc_id      = aws_vpc.vpc_webserver.id
+
+  ingress {
+    description = "Allow SSH traffic"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "Allow HTTP traffic"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
+#Create and bootstrap webserver
+resource "aws_instance" "nginx_webserver" {
+  ami                         = data.aws_ssm_parameter.webserver_ami.value
+  instance_type               = var.instance_type
+  key_name                    = aws_key_pair.key_webserver.key_name
+  associate_public_ip_address = true
+  vpc_security_group_ids      = [aws_security_group.sg_webserver.id]
+  subnet_id                   = aws_subnet.subnet_webserver.id
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt-get update",
+      "sudo apt-get -y install nginx",
+      "sudo systemctl start nginx",
+      "echo '<h1><center>Hello World of Terraform/Devops</center></h1>' > index.html",
+      "mv index.html /usr/share/nginx/html/"
+    ]
+
+    connection {
+      type        = "ssh"
+      user        = "ubuntu"
+      private_key = file("~/.ssh/id_rsa")
+      host        = self.public_ip
+    }
+
+    # tags = {
+    #   Name = var.instance_tag
+    # }
+  }
+}
+
+output "webserver_public_ip" {
+  value = aws_instance.nginx_webserver.public_ip
+}
+
+
+
+
+
+
+
+
+# Code GraveYard
+# --------------------
 
 # terraform {
 #   backend "s3" {
@@ -8,28 +152,28 @@ provider "aws" {}
 #   }
 # }
 
-resource "aws_vpc" "vpc_nginx" {
-  cidr_block           = var.vpc_cidr
-  enable_dns_support   = true
-  enable_dns_hostnames = true
-  #   ipv6_cidr_block = var.ipv6_cidr
+# resource "aws_vpc" "vpc_nginx" {
+#   cidr_block           = var.vpc_cidr
+#   enable_dns_support   = true
+#   enable_dns_hostnames = true
+#   #   ipv6_cidr_block = var.ipv6_cidr
 
-  tags = {
-    "Name" = "nginx_VPC"
-  }
-}
+#   tags = {
+#     "Name" = "nginx_VPC"
+#   }
+# }
 
 # data "aws_route" "main_route_table" {
 #   route_table_id = aws_default_route_table.route_nginx.id
-  # filter {
-  #   name   = "association.main"
-  #   values = ["true"]
-  # }
+# filter {
+#   name   = "association.main"
+#   values = ["true"]
+# }
 
-  # filter {
-  #   name   = "vpc_id"
-  #   values = [aws_vpc.vpc.id]
-  # }
+# filter {
+#   name   = "vpc_id"
+#   values = [aws_vpc.vpc.id]
+# }
 # }
 
 # resource "aws_default_route_table" "route_nginx" {
@@ -58,16 +202,16 @@ resource "aws_vpc" "vpc_nginx" {
 #   }
 # }
 
-resource "aws_subnet" "pub_sub" {
-  # vpc_id     = aws_vpc.vpc_nginx
-  # vpc_id     = aws_vpc.vpc_nginx_personal.id
-  vpc_id     = aws_vpc.vpc_nginx.id
-  cidr_block = var.pub_cidr
+# resource "aws_subnet" "pub_sub" {
+#   # vpc_id     = aws_vpc.vpc_nginx
+#   # vpc_id     = aws_vpc.vpc_nginx_personal.id
+#   vpc_id     = aws_vpc.vpc_nginx.id
+#   cidr_block = var.pub_cidr
 
-  tags = {
-    Name = var.sub_tag
-  }
-}
+#   tags = {
+#     Name = var.sub_tag
+#   }
+# }
 
 # resource "aws_internet_gateway" "nginx_ig" {
 #   vpc_id = var.vpc_id
@@ -78,41 +222,41 @@ resource "aws_subnet" "pub_sub" {
 # }
 
 
-resource "aws_security_group" "sg_nginx" {
-  name        = "nginx_sg_ssh"
-  description = "Allow TCP/80 && TCP/22 Traffic"
-  vpc_id      = var.vpc_id
-  # vpc_id = aws_vpc.vpc_nginx_personal.id
+# resource "aws_security_group" "sg_nginx" {
+#   name        = "nginx_sg_ssh"
+#   description = "Allow TCP/80 && TCP/22 Traffic"
+#   vpc_id      = var.vpc_id
+#   # vpc_id = aws_vpc.vpc_nginx_personal.id
 
 
-  ingress {
-    description = "Allow SSH Traffic"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+#   ingress {
+#     description = "Allow SSH Traffic"
+#     from_port   = 22
+#     to_port     = 22
+#     protocol    = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
 
-  ingress {
-    description = "Allow HTTP Traffic"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+#   ingress {
+#     description = "Allow HTTP Traffic"
+#     from_port   = 80
+#     to_port     = 80
+#     protocol    = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
 
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
+#   egress {
+#     from_port        = 0
+#     to_port          = 0
+#     protocol         = "-1"
+#     cidr_blocks      = ["0.0.0.0/0"]
+#     ipv6_cidr_blocks = ["::/0"]
+#   }
 
-  tags = {
-    "Name" = var.sg_tag
-  }
-}
+#   tags = {
+#     "Name" = var.sg_tag
+#   }
+# }
 
 # resource "aws_key_pair" "deployer" {
 #   key_name   = "provision_key"
@@ -120,78 +264,78 @@ resource "aws_security_group" "sg_nginx" {
 
 # }
 
-resource "tls_private_key" "key_pair" {
-  algorithm = "RSA"
-  rsa_bits  = 4096
-}
+# resource "tls_private_key" "key_pair" {
+#   algorithm = "RSA"
+#   rsa_bits  = 4096
+# }
 
-resource "aws_key_pair" "generated_key" {
-  # key_name = var.key_name
-  public_key = tls_private_key.key_pair.public_key_openssh
-}
+# resource "aws_key_pair" "generated_key" {
+#   # key_name = var.key_name
+#   public_key = tls_private_key.key_pair.public_key_openssh
+# }
 
-data "aws_ami" "nginx_ami" {
-  owners      = ["099720109477"]
-  most_recent = true
+# data "aws_ami" "nginx_ami" {
+#   owners      = ["099720109477"]
+#   most_recent = true
 
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/*"]
-  }
+#   filter {
+#     name   = "name"
+#     values = ["ubuntu/images/hvm-ssd/*"]
+#   }
 
-}
-
-
-resource "aws_instance" "nginx_server" {
-  ami           = data.aws_ami.nginx_ami.id
-  instance_type = var.instance_type
-  # subnet_id     = aws_subnet.pub_sub.id
-  subnet_id       = var.subnet_id
-  security_groups = [aws_security_group.sg_nginx.id]
-  key_name = aws_key_pair.generated_key.key_name
+# }
 
 
-  # connection {
-  #   type        = "ssh"
-  #   user        = "admin"
-  #   private_key = file("~/.ssh/id_rsa")
-  #   host        = self.public_ip
-  # }
-
-  provisioner "remote-exec" {
-    inline = [
-      "sudo apt-get update",
-      "sudo apt-get -y install nginx",
-      "sudo systemctl start nginx",
-      "echo '<h1><center>Hello World of Terraform/DevOps</center><h1>'  > index.html",
-      "sudo mv index.html /usr/share/nginx/html"
-    ]
-  }
-
-  # user_data = file("files/nginx_install.sh")
-
-  # provisioner "file" {
-  #   source = "./web/hello_world.html"
-  #   destination = "/var/www/"
-  # }
-
-  # depends_on = [
-  #   aws_internet_gateway.nginx_ig
-  # ]
+# resource "aws_instance" "nginx_server" {
+#   ami           = data.aws_ami.nginx_ami.id
+#   instance_type = var.instance_type
+#   # subnet_id     = aws_subnet.pub_sub.id
+#   subnet_id       = var.subnet_id
+#   security_groups = [aws_security_group.sg_nginx.id]
+#   key_name = aws_key_pair.generated_key.key_name
 
 
-  tags = {
-    Name = var.tags
-  }
-}
+# connection {
+#   type        = "ssh"
+#   user        = "admin"
+#   private_key = file("~/.ssh/id_rsa")
+#   host        = self.public_ip
+# }
+
+# provisioner "remote-exec" {
+#   inline = [
+#     "sudo apt-get update",
+#     "sudo apt-get -y install nginx",
+#     "sudo systemctl start nginx",
+#     "echo '<h1><center>Hello World of Terraform/DevOps</center><h1>'  > index.html",
+#     "sudo mv index.html /usr/share/nginx/html"
+#   ]
+# }
+
+# user_data = file("files/nginx_install.sh")
+
+# provisioner "file" {
+#   source = "./web/hello_world.html"
+#   destination = "/var/www/"
+# }
+
+# depends_on = [
+#   aws_internet_gateway.nginx_ig
+# ]
 
 
-output "nginx_public_ip" {
-  value = aws_instance.nginx_server.public_ip
-}
+#   tags = {
+#     Name = var.tags
+#   }
+# }
 
-output "private_key" {
-  value = tls_private_key.key_pair.private_key_pem
-  sensitive = true
-  
-}
+
+# output "nginx_public_ip" {
+#   value = aws_instance.nginx_server.public_ip
+# }
+
+# output "private_key" {
+#   value = tls_private_key.key_pair.private_key_pem
+#   sensitive = true
+
+# }
--- variables.tf
+++ variables.tf
@@ -3,68 +3,119 @@ variable "aws_region" {
   default     = "us-east-1"
 }
 
-variable "ami_image" {
-  description = "instance image value"
-  default     = "ami-052efd3df9dad4825"
+variable "key_name" {
+  description = "name of key"
+  default     = "webserver_key"
+
+}
+
+variable "vpc_cidr" {
+  description = "cidr range for webserver vpc"
+  default     = "10.0.0.0/16"
+}
+
+variable "vpc_tag" {
+  description = "name for VPC"
+  default     = "nginx-vpc"
+}
+
+variable "subnet_cidr" {
+  description = "cidr range for webserver vpc"
+  default     = "10.0.1.0/24"
 }
 
 variable "instance_type" {
-  description = "type of instance to be used"
-  default     = "t2.micro"
+  description = "type of instance desired"
+  default     = "t3.micro"
 }
 
-variable "subnet_id" {
-  description = "instance security group"
-  default     = "subnet-0e08d43043dfbf9d2"
+variable "instance_tag" {
+  description = "name for instance"
+  default     = "nginx_webserver"
+
 }
 
-variable "tags" {
-  description = "instance resource tag"
-  default     = "nginx_vm"
-}
 
-// This is the given VPC for Cloud Playground
-variable "vpc_id" {
-  description = "id of used VPC"
-  default     = "vpc-0618c027cb0648ef8"
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# variable "ami_image" {
+#   description = "instance image value"
+#   default     = "ami-052efd3df9dad4825"
+# }
+
+# variable "instance_type" {
+#   description = "type of instance to be used"
+#   default     = "t2.micro"
+# }
+
+# variable "subnet_id" {
+#   description = "instance security group"
+#   default     = "subnet-0e08d43043dfbf9d2"
+# }
+
+# variable "tags" {
+#   description = "instance resource tag"
+#   default     = "nginx_vm"
+# }
+
+# // This is the given VPC for Cloud Playground
+# variable "vpc_id" {
+#   description = "id of used VPC"
+#   default     = "vpc-0618c027cb0648ef8"
+# }
+
+# # variable "vpc_cidr" {
+# #   description = "cidr range for selected VPC"
+# #   default     = "172.31.64.0/20"
+# # }
 
 # variable "vpc_cidr" {
 #   description = "cidr range for selected VPC"
+#   default     = "172.31.0.0/16"
+
+# }
+
+# # variable "vpc_cidr_perso" {
+# #   description = "cidr range for personal VPC"
+# #   default     = "172.31.0.0/16"
+# # }
+
+# variable "pub_cidr" {
+#   description = "cidr range for subnet"
 #   default     = "172.31.64.0/20"
 # }
 
-variable "vpc_cidr" {
-  description = "cidr range for selected VPC"
-  default     = "172.31.0.0/16"
-
-}
-
-# variable "vpc_cidr_perso" {
-#   description = "cidr range for personal VPC"
-#   default     = "172.31.0.0/16"
+# variable "sg_tag" {
+#   description = "name for security group"
+#   default     = "port 22 && 80"
 # }
 
-variable "pub_cidr" {
-  description = "cidr range for subnet"
-  default     = "172.31.64.0/20"
-}
+# variable "sub_tag" {
+#   description = "name for public subnet"
+#   default     = "public_subnet"
 
-variable "sg_tag" {
-  description = "name for security group"
-  default     = "port 22 && 80"
-}
+# }
 
-variable "sub_tag" {
-  description = "name for public subnet"
-  default     = "public_subnet"
+# variable "ig_id" {
+#   description = "id value for desired internet gateway"
+#   default     = "igw-0d2a48b2658242edd"
 
-}
-
-variable "ig_id" {
-  description = "id value for desired internet gateway"
-  default     = "igw-0d2a48b2658242edd"
-
-}
+# }
 
 # variable "key_name" {}
